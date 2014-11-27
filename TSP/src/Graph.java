65ee54f9eb209ebd0b9354e64de732ec84470367
Andreacmj <andreas.cm.johansson@gmail.com>
2014-11-27 11:36:49 +0100

    Added TSPSolver Interface and began on Tests

---
+++ TSP/src/Graph.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Andreas on 2014-11-27.
+ */
+public class Graph {
+}
--- TSP/src/Test.java
+++ TSP/src/Test.java
@@ -2,4 +2,22 @@
  * Created by Andreas on 2014-11-27.
  */
 public class Test {
+
+    public static void main(String[] args) {
+        runTests();
+    }
+
+    private static void runTests() {
+
+        TSPSolver solver = new SimpleHeuristic();
+
+
+        assert (OnlyOneNodeTest(solver));
+    }
+
+
+    public static boolean OnlyOneNodeTest(TSPSolver solver) {
+        return true;
+    }
+
 }
